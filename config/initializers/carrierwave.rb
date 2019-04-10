5535f565cfdc9ad37b192ee8e4528e13727b558c
ViKriP <viktor_biz@ukr.net>
2019-04-10 16:06:17 +0300

    update carrierwave

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -5,7 +5,7 @@ CarrierWave.configure do |config|
       provider: 'AWS',
       aws_access_key_id: ENV['S3_KEY'],
       aws_secret_access_key: ENV['S3_SECRET'],
-      region: 'us-east-2',
+      region: 'us-east-1',
       path_style: true
     }
     config.storage = :fog
