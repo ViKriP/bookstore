cd94eb74d9c3b142015cd0d1f60e60a6f8b869c1
ViKriP <viktor_biz@ukr.net>
2019-09-09 16:29:34 +0300

    fix for heroku

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -1,10 +1,18 @@
 class ImageUploader < CarrierWave::Uploader::Base
   include CarrierWave::MiniMagick
 
+  if Rails.env.production?
+    storage :fog
+  end
+
   process resize_to_fit: [400, 400]
 
   version :thumb do
     process resize_to_fill: [150, 150]
+  end
+
+  def store_dir
+    "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
   end
 
   def default_url(*args)
