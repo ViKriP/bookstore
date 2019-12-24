f305e3ba0bc4e006a2403c486e8fa8b219cc74e4
ViKriP <viktor_biz@ukr.net>
2019-12-24 16:08:32 +0200

    reconfigured for Circleci

--- spec/presenters/homepage_presenter_spec.rb
+++ spec/presenters/homepage_presenter_spec.rb
@@ -2,8 +2,10 @@ require 'rails_helper'
 
 describe HomepagePresenter do
   describe '#last_books' do
+    let!(:books) { create_list(:book, 7) }
+
     it 'returns collection' do
-      expect(described_class.new.last_books).to be_a Array
+      expect(described_class.new.last_books.sort).to eql [ books[4], books[5], books[6] ]
     end
   end
 
